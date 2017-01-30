e34181ba25c6516b680b7d958743ef5b43c10a14
silver926z <silver926z@gmail.com>
2017-01-31 00:22:20 +0800

    add file
    2017.1.31

---
+++ python_training.py
@@ -0,0 +1,21 @@
+from itertools import *
+from Tkinter import *
+print 'test'
+# root = Tk()
+# some = Label(root, text="Tk's job!!", width="30", height="5")
+# some.pack()
+# root.mainloop()
+# 
+# for i in product('ABCD', repeat=3):
+#     print "".join(i),'@'
+# for i in permutations('ABCD',3):
+#     print "".join(i)
+
+# print 'xd'+str(5 if raw_input()=='1' else 2)
+
+# numbers = [10, 20, 30]
+# print [number ** 2 for number in numbers]
+
+# lts = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print [ele for lt in lts for ele in lt]
+
